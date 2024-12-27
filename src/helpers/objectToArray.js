cddbe275112dbe17f96522c84d40269e895ce5b2
Ciprian Mocanu <ciprian@mbe.ro>
2024-12-27 14:30:19 +0100

    Minor change required for the lock on the code customizer

--- src/helpers/objectToArray.js
+++ src/helpers/objectToArray.js
@@ -1,14 +1,14 @@
-import { reduce } from 'lodash';
-export const objectToArray = (obj = {}, count = 2) => {
+import { isUndefined, reduce } from 'lodash';
+export const objectToArray = (obj = {}, count = 2, keyToUse) => {
     // Create an empty array with a certain length
-    const final = [];
-    final[count - 1] = undefined;
+    const final = Array(count).fill(undefined);
 
     // Return the array
     return reduce(
         obj,
         (result, value, key) => {
-            result[key] = value;
+            const usedKey = isUndefined(keyToUse) ? key : value[keyToUse];
+            result[usedKey] = value;
             return result;
         },
         final
